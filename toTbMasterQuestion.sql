232356ecd7bb744d971c531a0a25f91bce94e44e
rmabini <rmabini@dairy-farm.com.ph>
2017-07-06 12:00:51 +0800

    Add additional query

--- toTbMasterQuestion.sql
+++ toTbMasterQuestion.sql
@@ -1,12 +1,56 @@
-﻿CREATE OR REPLACE FUNCTION toDeleteEntryTbMasterQuestionGrade(
-p_id_master_question integer integer
+﻿
+SELECT id_master_template 
+FROM tb_master_template 
+WHERE template_name ILIKE '%UPERMARKET%' limit 1 
+
+CREATE OR REPLACE FUNCTION getIdMasterTemplage
+(type character) RETURNS INTEGER AS
+$$
+DECLARE
+id integer;
+ BEGIN
+SELECT  id_master_template into id  
+FROM tb_master_template 
+WHERE template_name ILIKE ('%' || type || '%') limit 1;
+IF FOUND  THEN
+       RETURN id;
+    END IF;
+    RETURN 0; 
+ END
+$$
+LANGUAGE plpgsql VOLATILE;
+
+Select * from getIdMasterTemplage('UPERMARKET') as id_master_template ;
+
+
+CREATE OR REPLACE FUNCTION toAddEntryToTbMasterQuestionGrade(
+p_id_master_question integer,
+p_grade integer,
+p_gradeValue integer,
+p_created_by character varying(250)
 ) RETURNS VOID AS
 $$
 BEGIN
-END;
-$$
-LANGUAGE sql STABLE;
+IF p_grade > 0 THEN
+   INSERT INTO tb_master_question_grade(
+            id_master_question, id_grade, created_date, created_by)
+    VALUES (p_id_master_question, p_gradeValue, now(), p_created_by);
 
+END IF;
+END
+$$
+LANGUAGE plpgsql VOLATILE;
+
+CREATE OR REPLACE FUNCTION toDeleteEntryTbMasterQuestionGrade(
+p_id_master_question integer ) RETURNS VOID AS
+$$
+ Delete From tb_master_question_grade where id_master_question = $1;
+$$
+LANGUAGE sql VOLATILE;
+
+
+Select * from toDeleteEntryTbMasterQuestionGrade(1);
+Select * from tb_master_question_grade
 
 CREATE OR REPLACE FUNCTION toTbMasterQuestion(
   p_id_master_question integer,
@@ -92,6 +136,35 @@ Select * from staging, totbmasterquestion(
     );
 
 Select * from now();
+
+
+CREATE TABLE staging
+(
+  id_master_question serial NOT NULL,
+  type character varying(255),
+  section character(50),
+  "number" integer,
+  question_title character varying(255),
+  question_content text,
+  weight integer,
+  is_na boolean,
+  "G4" integer,
+  "G3" integer,
+  "G2" integer,
+  "G0" integer,
+  question_title_vietnamese character varying(255),
+  question_content_vietnamese text,
+  question_title_bahasa character varying(255),
+  question_content_bahasa text,
+  question_title_chinese character varying(255),
+  question_content_chinese text,
+  CONSTRAINT staging_pkey PRIMARY KEY (id_master_question)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE staging
+  OWNER TO postgres;
 
 COPY staging
 FROM 'D:/Test3.csv'
